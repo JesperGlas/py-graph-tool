0c3986e84bf3d4d4b7bb6e5cc623c2b7b1d0c6f4
jesperglas <jesper.glas@gmail.com>
2023-01-19 16:11:00 +0100

    Update

--- src/Graph.py
+++ src/Graph.py
@@ -4,9 +4,9 @@ from math import sqrt, pow
 class Graph:
     
     def __init__(self):
-        self._vertices: Dict[str, Tuple[int, int]] = {}
         self._edges: Dict[str, set[str]] = {}
         self._weights: Dict[str, float] = {}
+        self._positions: Dict[str, Tuple[int, int]] = {}
 
     def __str__(self) -> str:
         return f"""
@@ -14,15 +14,11 @@ class Graph:
         E={self.edges}
         """
     def __len__(self) -> int:
-        return len(self.vertices)
-
-    @property
-    def vert_data(self) -> Dict[str, Tuple[int, int]]:
-        return self._vertices
+        return len(self._edges)
 
     @property
     def vertices(self) -> set[str]:
-        return set(self._vertices.keys())
+        return set(self._edges.keys())
     
     @property
     def edges(self) -> set[str]:
@@ -36,57 +32,53 @@ class Graph:
     def weight_data(self) -> Dict[str, float]:
         return self._weights
 
-    def get_pos(self, vert_key: str) -> Tuple[int, int]:
-        return self.vert_data[vert_key]
-
-    def adjacent(self, vertex: str) -> set[str]:
-        return self.vert_data[vertex]
+    @property
+    def pos_data(self) -> Dict[str, Tuple[int, int]]:
+        return self._positions
     
-    def add_vert(self, position: Tuple[int, int]) -> None:
-        vert_key = self.vert_key(position)
-        self.vert_data[vert_key] = position
-        self.edge_data[vert_key] = set()
+    def adjacent(self, vert_key: str) -> set[str]:
+        return self._edges[vert_key]
+
+    def get_pos(self, vert_key: str) -> Tuple[int, int]:
+        return self.pos_data[vert_key]
+    
+    def add_vert(self, vert_key: str, pos: Tuple[int, int]=None) -> None:
+        self._edges[vert_key] = set()
+        self._positions[vert_key] = pos
     
     def remove_vert(self, vert_key: str) -> None:
         for src_key, adjacent in self.edge_data.items():
             if vert_key in adjacent:
-                self.vert_data[src_key].remove(vert_key)
-        if vert_key in self.vertices:
-            del self.vert_data[vert_key]
+                self._edges[src_key].remove(vert_key)
+                del self._weights[self._edge_key(src_key, vert_key)]
+        if vert_key in self._edges.keys():
             del self.edge_data[vert_key]
     
     def add_edge(self, src_key: str, dst_key: str, weight: float=None, directed: bool=False) -> None:
         # make sure vertices are in graph
-        if src_key not in self.vertices:
-            print(f"Source node: {src_key} not found in graph.. (Tip: add_vert)")
-            return
+        if src_key not in self._edges.keys():
+            self.add_vert(src_key)
 
         if dst_key not in self.vertices:
-            print(f"Destination node: {dst_key} not found in graph.. (Tip: add_vert)")
+            self.add_vert(dst_key)
 
         # add directed edge
         self._edges[src_key].add(dst_key)
-        self._weights[self.edge_key(src_key, dst_key)] = weight if weight != None else self.distance(src_key, dst_key)
-
+        self._weights[self.edge_key(src_key, dst_key)] = weight 
         # add reversed edge (omni-directional)
         if directed == False:
             self._edges[dst_key].add(src_key)
-            self._weights[self.edge_key(dst_key, src_key)] = weight if weight != None else self.distance(src_key, dst_key)
+            self._weights[self.edge_key(dst_key, src_key)] = weight 
 
     def clear(self) -> None:
-        self.vert_data.clear()
         self.edge_data.clear()
         self.weight_data.clear()
+        self.pos_data.clear()
 
-    def vert_key(self, position: Tuple[int, int]) -> str:
-        return f"{position}"
-    
     def edge_key(self, src: str, dst: str) -> str:
         return f"{src}->{dst}"
 
     def distance(self, src_key: str, dst_key: str) -> float:
-        src: Tuple[int, int] = self.get_pos(src_key)
-        dst: Tuple[int, int] = self.get_pos(dst_key)
-        print(f"Src: {src}({type(src)}) -> Dst: {dst}({type(dst)})")
-        return sqrt(pow(src[0] - dst[0], 2) + pow(src[1] - dst[1], 2))
-    
+        src_x, src_y = self.pos_data[src_key]
+        dst_x, dst_y = self.pos_data[dst_key]
+        return sqrt(pow(src_x - dst_x, 2) + pow(src_y - dst_y, 2))
--- src/main.py
+++ src/main.py
@@ -7,11 +7,10 @@ GRAPH   = Graph()
 PADDING = 10
 VIEW_WIDTH = 800
 VIEW_HEIGHT = 600
-ITEMS: Dict[str, set[str]] = {
-    "vertices": set(),
-    "edges": set(),
-    "selected": set()
-}
+VERTS: set[str] = set()
+EDGES: set[str] = set()
+SEL: set[str] = set()
+HL: set[str] = set()
 
 # help variables
 LEFT_CLICK = 0
@@ -71,71 +70,90 @@ def __mouse_move_callback(sender, data):
 def __mouse_click_callback(sender: str, data):
     print(f"[Mouse Click] Sender: {sender}, Data: {data}")
     if data == RIGHT_CLICK:
-        GRAPH.add_vert(dpg.get_value("val:mouse_position")[:2])
+        mouse_pos = dpg.get_value("val:mouse_position")[:2] 
+        vert_key = __vert_key(mouse_pos)
+        GRAPH.add_vert(vert_key)
         __load_graph()
 
 def __vert_btn_callback(sender: str, data):
     print(f"[Vert Btn] Sender: {sender}, Data: {data}")
-    if (selected := sender.split(":")[1]) not in list(ITEMS["selected"]):
-        ITEMS["selected"].add(selected)
+    if (selected := sender.split(":")[1]) not in list(SEL):
+        SEL.add(selected)
     else:
-        ITEMS["selected"].remove(selected)
+        SEL.remove(selected)
     __load_graph()
+    return
 
 def __reset_graph_callback(sender, data):
     GRAPH.clear()
     __load_graph()
+    return
 
 def __remove_btn_callback(sender: str, data):
     print(f"[Remove Btn] Sender: {sender}, Data: {data}")
-    if len(selection := ITEMS["selected"]) > 0:
-        for vert_key in selection:
+    if len(SEL) > 0:
+        for vert_key in SEL:
             GRAPH.remove_vert(vert_key)
         __load_graph()
+    return
 
 def __connect_btn_callback(sender: str, data):
     print(f"[Connect Btn] Sender: {sender}, Data: {data}")
-    if len(ITEMS["selected"]) >= 2:
-        for i, src in enumerate(ITEMS["selected"]):
-            for dst in list(ITEMS["selected"])[i:]:
+    if len(SEL) >= 2:
+        for i, src in enumerate(SEL):
+            for dst in list(SEL)[i:]:
                 GRAPH.add_edge(src, dst)
     print(GRAPH)
     __load_edges()
+    return
 
 def __load_graph():
     __load_vertices()
+    __load_edges()
+    return
 
 def __load_vertices():
-    for vert_key in GRAPH.vertices.union(ITEMS["vertices"]):
-        if vert_key in GRAPH.vertices and vert_key not in ITEMS["vertices"]:
-            ITEMS["vertices"].add(vert_key)
+    for vert_key in GRAPH.vertices.union(VERTS):
+        if vert_key in GRAPH.vertices and vert_key not in VERTS: 
+            VERTS.add(vert_key)
             dpg.draw_circle(tag=f"vert:{vert_key}", parent="canvas:main", center=dpg.get_value("val:mouse_position")[:2], radius=5, fill=(0, 255, 0, 255))
             dpg.add_checkbox(tag=f"btn:{vert_key}", label=vert_key, parent="menu:vertices", callback=__vert_btn_callback)
-        if vert_key not in GRAPH.vertices and vert_key in ITEMS["vertices"]:
+        if vert_key not in GRAPH.vertices and vert_key in VERTS:
             dpg.delete_item(f"vert:{vert_key}")
             dpg.delete_item(f"btn:{vert_key}")
-            ITEMS["vertices"].remove(vert_key)
-            if vert_key in ITEMS["selected"]: ITEMS["selected"].remove(vert_key)
-        if vert_key in ITEMS["selected"]:
+            VERTS.remove(vert_key)
+            if vert_key in SEL:
+                SEL.remove(vert_key)
+        if vert_key in SEL:
             dpg.configure_item(f"vert:{vert_key}", fill=(255, 0, 0, 255))
+    return
 
 def __load_edges():
+    print("Loading edges..")
     for src_key, adjacent in GRAPH.edge_data.items():
         for dst_key in adjacent:
-            if (edge_key := GRAPH.edge_key(src_key, dst_key)) not in ITEMS["edges"] and GRAPH.edge_key(dst_key, src_key) not in ITEMS["edges"]:
-                ITEMS["edges"].add(edge_key)
-                src = GRAPH.get_pos(src_key)
-                dst = GRAPH.get_pos(dst_key)
+            if (edge_key := GRAPH.edge_key(src_key, dst_key)) not in EDGES and GRAPH.edge_key(dst_key, src_key) not in EDGES:
+                EDGES.add(edge_key)
+                src = __get_pos(src_key)
+                dst = __get_pos(dst_key)
                 dpg.draw_line(src, dst, color=(0, 0, 255, 255), thickness=1, parent="canvas:main")
+    return
             
+def __get_pos(vert_key: str) -> Tuple[int, int]:
+    return GRAPH.get_pos(vert_key)
+
+def __vert_key(position: Tuple[int, int]) -> str:
+    return f"{position}"
 
 def run():
     dpg.start_dearpygui()
     dpg.destroy_context()
+    return
 
 def main():
     init()
     run()
+    return
     
 if __name__ == "__main__":
     main()
